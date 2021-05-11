a5f7bacbc0b015b977888438e2d37549e183dc21
frunox <jrcannon@msn.com>
2021-05-11 12:55:34 -0500

    set local env

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAMrBhlzOVE2AJjjkL10xB0JJxBnNGMkQU',
-  authDomain: 'daily-moments-df7aa.firebaseapp.com',
-  projectId: 'daily-moments-df7aa',
-  storageBucket: 'daily-moments-df7aa.appspot.com',
-  messagingSenderId: '757112058055',
-  appId: '1:757112058055:web:80282e2be0c04aeda54ee2',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = firebase.initializeApp(firebaseConfig);
